0f8ac7b12a95574f8c6ef68ceb1da29b70982ea5
Gerardo Rodriguez <gerardor.com@gmail.com>
2014-06-09 01:00:57 -0300

    first commit

---
+++ config.rb
@@ -0,0 +1,5 @@
+css_dir = 'css'
+sass_dir = 'componentes/sass'
+javascripts_dir = 'js'
+output_style = :nested
+relative_assets = true
---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt){
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.initConfig({
+    uglify:{
+      my_target:{
+        files: {
+          'js/ford1.js':['componentes/js/*.js']
+        }//files
+      }//my_target
+    },//uglify
+    compass: {
+      dev: {
+        config: 'config.rb'
+      }//options
+    },//dev
+    watch:{
+      options: { livereload: true },
+      scripts: {
+        files: ['componentes/js/*.js'],
+        tasks: ['uglify']
+      },//scripts
+      sass: {
+        files: ['componentes/sass/*.scss'],
+        tasks: ['compass:dev']
+      },//sass
+      html: {
+        files: ['*.html']
+      }
+    }//watch
+  })//initConfig
+  grunt.registerTask('default', 'watch');
+} //exports
